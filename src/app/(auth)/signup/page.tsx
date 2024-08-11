7db4915841ce5823be2cfcbc77194f69c731e22d
fajaredip <fajar.e@elabram.com>
2024-08-12 06:21:59 +0700

    auth page - signup

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,40 @@
+import { Metadata } from "next";
+import signUpImage from "@/assets/signup-image.jpg";
+import Image from "next/image";
+import Link from "next/link";
+import SignUpForm from "./signUpForm";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
+
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center p-5">
+      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-md bg-card shadow-md">
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+          <div className="space-y-1 text-center">
+            <h1 className="text-3xl font-bold">Sign Up to Kicaw</h1>
+            <p className="text-muted-foreground">
+              A place where even you can find a friend.
+            </p>
+          </div>
+          <div className="space-y-5">
+            <SignUpForm />
+            <div className="block text-center">
+              Already have an account?
+              <Link href="/login" className="hover:underline">
+                Log in
+              </Link>
+            </div>
+          </div>
+        </div>
+        <Image
+          src={signUpImage}
+          alt=""
+          className="hidden w-1/2 object-cover md:block"
+        />
+      </div>
+    </main>
+  );
+}
